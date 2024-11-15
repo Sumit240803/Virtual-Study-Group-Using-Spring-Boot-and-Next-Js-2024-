a7f3e0f57b35f2c9f24987e0b11d052914b81ae2
Sumit240803 <goyalsumit651@gmail.com>
2024-11-15 13:21:17 +0530

    Hosting 1.4

--- learningtool_frontend/app/pages/auth/login/page.js
+++ learningtool_frontend/app/pages/auth/login/page.js
@@ -18,7 +18,7 @@ const Page = () => {
     e.preventDefault();
     //console.log(formData);
     try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
+      const response = await fetch(`https://virtual-study-group-using-spring-boot.onrender.com/auth/login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),

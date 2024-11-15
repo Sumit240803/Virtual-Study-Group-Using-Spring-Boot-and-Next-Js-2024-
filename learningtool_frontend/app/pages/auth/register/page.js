f1ae8a1681115cb8ebfa48f0e473347fb0c338bc
Sumit240803 <goyalsumit651@gmail.com>
2024-11-16 01:20:40 +0530

    Connection Resolving 1.8

--- learningtool_frontend/app/pages/auth/register/page.js
+++ learningtool_frontend/app/pages/auth/register/page.js
@@ -25,6 +25,7 @@ const Page = () => {
       const response = await fetch(`https://virtual-study-group-using-spring-boot.onrender.com/auth/register`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
+        mode : "no-cors",
         body: JSON.stringify(formData),
       });
       if (response.ok) {
